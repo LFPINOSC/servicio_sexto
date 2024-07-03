452d1c0e24acdfaa627d6e82c82a94cee2249fda
BRYAN SANTIAGO MULLO LEMA <bryan.mullo.70@est.ucacue.edu.ec>
2024-07-02 20:21:22 -0500

    crear clase dueño

---
+++ src/models/Dueño.js
@@ -0,0 +1,37 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/sequelizeConfig.js';
+
+const Dueño = sequelize.define('Dueño', {
+  Cedula: {
+    type: DataTypes.STRING(20),
+    primaryKey: true,
+
+  },
+  Nombre: {
+    type: DataTypes.STRING(75),
+    allowNull: false,
+    field: 'Nombre',
+  },
+  Apellido: {
+    type: DataTypes.STRING(30), 
+    allowNull: false,
+    field:"Apellido",
+  },
+  Telefono: {
+    type: DataTypes.STRING(20), 
+    allowNull: false,
+    field:"Telefono",
+  },
+  Direccion: {
+    type: DataTypes.STRING(50), 
+    allowNull: false,
+    field:"Direccion",
+  },
+  estaActivo: {
+    type: DataTypes.SMALLINT, 
+    allowNull: false,
+    defaultValue: 1,
+  },
+  // Otros campos de tipo de usuario
+});
+export default TipoUsuario;
