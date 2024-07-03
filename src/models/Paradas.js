d8c985449a50b5cbb5475d8b58cee84f637d6cd8
MSI <luisarcentales7@gmail.com>
2024-07-02 20:14:45 -0500

    Atributos de la clase de Paradas

---
+++ src/models/Paradas.js
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/sequelizeConfig.js';
+
+const TipoParada = sequelize.define('Parada', {
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    autoIncrementIdentity: true,
+  },
+  Nombre: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    field: 'Nombre',
+  },
+  CordenadaX: {
+    type: DataTypes.GEOMETRY, 
+    allowNull: false,
+    defaultValue: 1,
+  },
+  CordenadaY: {
+    type: DataTypes.GEOMETRY, 
+    allowNull: false,
+    defaultValue: 1,
+  },
+  estaActivo: {
+    type: DataTypes.SMALLINT, 
+    allowNull: false,
+    defaultValue: 1,
+  },
+  // Otros campos de tipo de usuario
+});
+export default TipoUsuario;
